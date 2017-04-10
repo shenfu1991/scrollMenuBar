5c699867d39223ffc74a11165e83c2fb69788a25
shenfu1991 <xuanyuantiandipan@163.com>
2017-04-10 18:23:17 +0800

    Update ScrollMenuView.h

--- sliderMenu/sliderMenu/ScrollMenuView.h
+++ sliderMenu/sliderMenu/ScrollMenuView.h
@@ -15,13 +15,16 @@
 @end
 
 @interface ScrollMenuView : UIView
+
+/**
+ 每个项目中的间距
+ */
+@property (assign,nonatomic)CGFloat itemPadding;
 @property (nonatomic,weak)id<ScrollMenuViewDelegate>delegate;
 
 
 /**
  添加项目
-
- @param items <#items description#>
  @param color 正常标题颜色
  @param selectedColor 选中标题颜色
  */
